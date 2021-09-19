9363a2e123e8a1bf44300a52eef419a86d0e9507
Muhammad Thabsheer N P <mthabsheernp@gmail.com>
2021-09-19 16:34:24 +0530

    Added columns widget and added containers to show data

--- mobile_portfolio_app/lib/containerWidgets.dart
+++ mobile_portfolio_app/lib/containerWidgets.dart
@@ -7,8 +7,8 @@ class Layout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.8,
-        height: MediaQuery.of(context).size.height * 0.8,
+        width: MediaQuery.of(context).size.width * 0.75,
+        height: MediaQuery.of(context).size.height * 0.75,
         decoration: BoxDecoration(
           color: Colors.greenAccent[400],
           borderRadius: BorderRadius.circular(10),
@@ -17,6 +17,81 @@ class Layout extends StatelessWidget {
               color: Colors.green,
               blurRadius: 10.0,
               spreadRadius: 1.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              width: 300,
+              height: 200,
+              decoration: BoxDecoration(
+                // color: Colors.grey[100],
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade100,
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                // color: Colors.grey[100],
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade100,
+                    width: 2,
+                  ),
+                  // top: BorderSide(
+                  //   color: Colors.indigo.shade900,
+                  //   width: 2,
+                  // ),
+                ),
+              ),
+              child: Center(
+                child: ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text("LinkedIn"),
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                // color: Colors.grey[100],
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade100,
+                    width: 2,
+                  ),
+                  // top: BorderSide(
+                  //   color: Colors.indigo.shade900,
+                  //   width: 2,
+                  // ),
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              // decoration: BoxDecoration(
+              //   // color: Colors.grey[100],
+              //   border: Border(
+              //     bottom: BorderSide(
+              //       color: Colors.indigo.shade900,
+              //       width: 2,
+              //     ),
+              //     // top: BorderSide(
+              //     //   color: Colors.indigo.shade900,
+              //     //   width: 2,
+              //     // ),
+              //   ),
+              // ),
             ),
           ],
         ),
