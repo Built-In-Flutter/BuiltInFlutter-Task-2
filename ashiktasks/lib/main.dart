02a8056c2883c51b7649e627a13daa673345745e
Ashik-Jose <ashiksholly@gmail.com>
2021-09-21 23:45:12 +0530

    Task 2 Completed

---
+++ ashiktasks/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:ashiktasks/Portfolio.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Portfolio(),
+    );
+  }
+}
